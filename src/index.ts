3561b0b6f29684a67ce5640e00e2aeeeba68e7a9
NLuisA <luis_elpara@hotmail.com.ar>
2025-07-22 01:13:40 -0300

    configuraciones  conexion mongoDB

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
+import colors from 'colors'
 import server from './server'
 const port = process.env.PORT || 4000 // en el local funciona con el puerto 4000 pero en production process.env.PORT si tiene 
 server.listen(port, () => {
-    console.log('Servidor funcionando en puerto:', port)
+    console.log(colors.bgGreen.italic('Servidor funcionando en puerto:'), port)
 })
