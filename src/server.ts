5063e5de16e8cb4acb642df6609ba5f38439228f
NLuisA <luis_elpara@hotmail.com.ar>
2025-07-22 00:56:31 -0300

    configuraciones  conexion mongoDB

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,10 @@
 import express from 'express' // ESM Ecmascript modules, es la forma de importar 
+import 'dotenv/config'
 import router from './rourter'
+import { connectDB } from './config/db' // importamos la conexion de mongo al server
 
 const app = express()
+connectDB()
 // Leer formulario
 app.use(express.json())
 app.use('/',router) // la funcion use detecta todas la url del rourt
