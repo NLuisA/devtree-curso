45c3314c23a93907b2d9337f91c974713c53e96c
NLuisA <luis_elpara@hotmail.com.ar>
2025-08-18 22:55:48 -0300

    registro de usuario realizado

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,15 @@
 import express from 'express' // ESM Ecmascript modules, es la forma de importar 
+import cors from 'cors'
 import 'dotenv/config'
 import router from './rourter'
 import { connectDB } from './config/db' // importamos la conexion de mongo al server
+import { corsConfig } from './config/cors' //importamos el cors para permitir la conexion
 
-const app = express()
 connectDB()
+const app = express()
+
+// cors 
+app.use(cors(corsConfig))
 // Leer formulario
 app.use(express.json())
 app.use('/',router) // la funcion use detecta todas la url del rourt
